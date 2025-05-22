751be927ad0f7b975ea02738e8270e9b84d3f6da
Hydroflame521 <hydroflame521.business@gmail.com>
2025-05-22 16:50:12 -0500

    updates

--- assets/scripts/copyall.js
+++ assets/scripts/copyall.js
@@ -2,7 +2,9 @@ document.querySelectorAll(".copy").forEach(copyButton => {
     copyButton.addEventListener("click", () => {
         const targetElement = document.querySelector(copyButton.dataset.copy);
         const textToCopy = targetElement.textContent;
-        navigator.clipboard.write(textToCopy)
+        navigator.clipboard.write(textToCopy).then(() => {
+            alert("Copied!")
+        })
         console.log(textToCopy);
     });
 });
